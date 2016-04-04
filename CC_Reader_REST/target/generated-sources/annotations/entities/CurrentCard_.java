0248d5e4a225e7b2fc3362d345239778e374f86c
RicardoSilva91 <ricardos897@hotmail.com>
2016-04-04 19:17:23 +0000

    new link on index

--- CC_Reader_REST/target/generated-sources/annotations/entities/CurrentCard_.java
+++ CC_Reader_REST/target/generated-sources/annotations/entities/CurrentCard_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-04-03T20:14:22")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-04-03T20:47:15")
 @StaticMetamodel(CurrentCard.class)
 public class CurrentCard_ { 
 
--- CC_Reader_REST/target/generated-sources/annotations/entities/Person_.java
+++ CC_Reader_REST/target/generated-sources/annotations/entities/Person_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-04-03T20:14:22")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-04-03T20:47:15")
 @StaticMetamodel(Person.class)
 public class Person_ { 
 

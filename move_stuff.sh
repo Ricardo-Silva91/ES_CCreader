2c5ae0967648c47671bb66be9582b83f801606e1
RicardoSilva91 <ricardos897@hotmail.com>
2016-04-07 00:28:50 +0000

    bug found, card reader stops being able to read cards properly if wrong type of card is inserted

---
+++ move_stuff.sh
@@ -0,0 +1,7 @@
+cp Card_Reader/dist/Card_Reader.jar full_releases/jetty/000_CC_reader_dist/Card_Reader.jar
+
+ cp CC_Reader_REST/target/CC_Reader_REST.war full_releases/jetty/000_CC_reader_dist/CC_Reader_REST.war
+
+ cp -r CC_Reader_REST/target/CC_Reader_REST/ full_releases/jetty/000_CC_reader_dist/CC_Reader_REST
+
+cp rabbitMQ_clientTest/dist/rabbitMQ_clientTest.jar full_releases/broker/rabbitMQ_clientTest.jar 
